fa5103f2587e536a1922a12337cb8641135dc196
zaifer <zaif3r@pm.me>
2023-05-09 13:31:23 -0500

    add ERC20TemplateFlatten

--- pages/api/templates/erc20.ts
+++ pages/api/templates/erc20.ts
@@ -7,20 +7,37 @@ import path from "path";
 
 import type { ERC20TemplateArgs } from "@/types/templates";
 
+const contractsDir = path.resolve(process.cwd(), "contracts");
+
+const templateFile = fs.readFileSync(
+    path.join(contractsDir, "ERC20Template.hbs"),
+    "utf8"
+);
+
+const sourceCodeFile = fs.readFileSync(
+    path.join(contractsDir, "ERC20TemplateFlatten.hbs"),
+    "utf8"
+);
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { method } = req;
     switch (method) {
         case "POST":
             try {
-                const contractsDir = path.resolve(process.cwd(), "contracts");
-                const data: ERC20TemplateArgs = JSON.parse(req.body);
-                const file = fs.readFileSync(
-                    path.join(contractsDir, "ERC20Template.hbs"),
-                    "utf8"
-                );
-                const template = Handlebars.compile(file);
-                const rendered = template(data);
-                res.json({ rendered });
+                const body: ERC20TemplateArgs = JSON.parse(req.body);
+                const decimals = body.decimals ? body.decimals : 18;
+                const data = {
+                    ...body,
+                    preMint: body.preMint ? body.preMint * 10 ** decimals : undefined,
+                    maxSupply: body.maxSupply
+                        ? body.maxSupply * 10 ** decimals
+                        : undefined,
+                };
+                const template = Handlebars.compile(templateFile);
+                const sourceCode = Handlebars.compile(sourceCodeFile);
+                const renderedTemplate = template(data);
+                const renderedSourceCode = sourceCode(data);
+                res.json({ template: renderedTemplate, sourceCode: renderedSourceCode });
             } catch (_error) {
                 console.log(_error);
                 res.status(400).end("error rendering erc20 template");
