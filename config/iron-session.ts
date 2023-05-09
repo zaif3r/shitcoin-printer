a42aedab54f215bfd31c40dc3d0d56b5c2ddca71
zaifer <zaif3r@pm.me>
2023-05-09 15:39:11 -0500

    add session password placeholder

--- config/iron-session.ts
+++ config/iron-session.ts
@@ -2,7 +2,7 @@ import { siteConfig } from "@/config/site"
 
 export const ironOptions = {
 	cookieName: siteConfig.name,
-	password: process.env.SESSION_PASSWORD!!,
+	password: process.env.SESSION_PASSWORD ?? "set_a_complex_password_at_least_32_characters_long",
 	cookieOptions: {
 		secure: process.env.NODE_ENV === 'production',
 	},
